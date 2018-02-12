2a024f6ade44d960a6a5ca20f609ae59e07f0917
nepeat <nepeat@users.noreply.github.com>
2018-02-11 21:52:22 -0800

    Refetch the best TX from the daemon after doing a send.

--- daemon.py
+++ daemon.py
@@ -14,6 +14,7 @@ from pycoin.key.validate import netcode_and_type_for_text
 from pycoin.tx.pay_to import ScriptPayToAddress, ScriptNulldata
 from pycoin.serialize import b2h, b2h_rev, h2b, h2b_rev
 import struct
+import time
 
 STATIC_FEE = float(os.environ.get("COIN_FEE", 0.01))
 NETWORKS = [
@@ -46,12 +47,23 @@ class CoinDaemon:
         rawtx = self.connection.getrawtransaction(txhash)
         return self.connection.decoderawtransaction(rawtx)
 
-    def find_best_txout_for_addr(self, address: str, vouts):
+    def find_best_txout_for_addr(self, address: str):
         best_value = 0
         index = -1
         script = ""
 
-        for output in vouts:
+        # Check for unspents.
+        unspents = self.connection.listunspent(0, 9999999, [address])
+        if not unspents:
+            raise Exception(f"No unspent inputs found on adddress {address}.")
+
+        # Store some unspents data.
+        best_tx = unspents[0]
+        tx_id = unspents[0]["txid"]
+        last_tx_decoded = self.decode_raw_tx_from_tx(best_tx["txid"])
+
+        # Parse vouts
+        for output in last_tx_decoded["vout"]:
             if address not in output["scriptPubKey"].get("addresses", []):
                 continue
 
@@ -63,26 +75,20 @@ class CoinDaemon:
         if index == -1:
             raise Exception("Could not find any TXouts for address.")
 
-        return index, script, best_value
+        return tx_id, index, script, best_value
 
-    def make_txs(self, address: str, chunker: Chunker, use_sends: bool=False) -> bool:
+    def make_txs(self, address: str, chunker: Chunker, use_sends: bool=False, dont_yield=None) -> bool:
+        i = 0
+        if not dont_yield:
+            dont_yield = []
+
         # We'll do our own chunking if we have to send to address hashes.
         if use_sends:
             chunker.chunk_size = 1024 * 8
         
         total_fee_needed = self.fee(chunker.data_size)
 
-        # Check for unspents.
-        unspents = self.connection.listunspent(0, 9999999, [address])
-        if not unspents:
-            raise Exception(f"No unspent inputs found on adddress {address}.")
-
-        # Store some unspents data.
-        last_tx_in = unspents[0]
-        last_tx_id = unspents[0]["txid"]
-        last_tx_decoded = self.decode_raw_tx_from_tx(last_tx_in["txid"])
-
-        last_index, last_script, last_amount = self.find_best_txout_for_addr(address, last_tx_decoded["vout"])
+        last_tx_id, last_index, last_script, last_amount = self.find_best_txout_for_addr(address)
         last_amount = float(last_amount) * 1000000
 
         # Calculate hash160 for later.
@@ -90,25 +96,18 @@ class CoinDaemon:
 
         # Calculate required TXes and their fees.
         needed_txes = chunker.chunk_count
-        # print(f"{needed_txes} TXs required.")
+        print(f"{needed_txes} TXs required.")
 
         required_coins = (needed_txes * total_fee_needed)
         if required_coins >= last_amount:
             raise Exception(f"Not enough coins to cover insertions. ({required_coins} or greater needed.)")
 
         for payload in chunker.generate_return_payloads():
-            fee_needed = self.fee(len(payload))
+            last_tx_id, last_index, last_script, last_amount = self.find_best_txout_for_addr(address)
+            last_amount = float(last_amount) * 1000000
+            fee_needed = self.fee(len(payload)) * 2
 
-            # txout
-            if use_sends:
-                gen_function = self.generate_addr_txouts
-            else:
-                gen_function = self.generate_return_txouts
-
-            last_index, txs_out = gen_function(address_hash160, payload, last_amount, fee_needed)
-            last_amount = last_amount - fee_needed - (self.return_fee * (len(txs_out) - 1))
-
-            # Generate the txin
+            # txin generate
             spendables = [Spendable.from_dict(dict(
                 coin_value=last_amount,
                 script_hex="0000",
@@ -118,11 +117,21 @@ class CoinDaemon:
 
             txs_in = [spendable.tx_in() for spendable in spendables]
 
+            # txout generate
+            if use_sends:
+                gen_function = self.generate_addr_txouts
+            else:
+                gen_function = self.generate_return_txouts
+
+            txs_out = gen_function(address_hash160, payload, last_amount, fee_needed)
+
+            # tx generate
             new_tx = Tx(1, txs_in, txs_out)
 
-            last_tx_id = new_tx.id()
+            if i not in dont_yield:
+                yield new_tx.as_hex(with_time=True)
 
-            yield new_tx.as_hex(with_time=True)
+            i += 1
 
     def generate_addr_txouts(self, address_hash160, _payload, last_amount, fee_needed):
         txs_out = []
@@ -150,7 +159,7 @@ class CoinDaemon:
             script_data = ScriptPayToAddress(hash160=chunk).script()
             txs_out.append(TxOut(self.return_fee, script_data))
 
-        return 0, txs_out
+        return txs_out
 
     def generate_return_txouts(self, address_hash160, payload, last_amount, fee_needed):
         txs_out = []
@@ -161,7 +170,7 @@ class CoinDaemon:
         script_pay = ScriptPayToAddress(hash160=address_hash160).script()
         txs_out.append(TxOut(last_amount - fee_needed - self.return_fee, script_pay))
         
-        return 1, txs_out
+        return txs_out
 
     def read_tx(self, txhash: str):
         result = ""
@@ -198,9 +207,17 @@ if __name__ == "__main__":
     else:
         raise Exception("environ not set TEST_DISCORD/TEST_FILE")
 
-
     coind = CoinDaemon()
 
-    for transaction in coind.make_txs("Se32GfsJuu76DLmupTWMKpWPtujYrBvfxi", test_trans, True):
-        print(transaction)
+    for transaction in coind.make_txs("SaryWoh3Aqrzk9r78uG7iBSTMPfz2suvGj", test_trans, True, dont_yield=[0]):
+        signed_tx = coind.connection.signrawtransaction(transaction)["hex"]
+        while True:
+            try:
+                sent_tx = coind.connection.sendrawtransaction(signed_tx, 1)
+                break
+            except Exception as e:
+                print("Failed to send TX. (%s)" % (e))
+                time.sleep(30)
+        print(sent_tx)
+
     # print(coind.read_tx("64e3262d4b7c6d528b972d84a6a7667bb07ba0373f68b8ee32d114101fb6b676"))
