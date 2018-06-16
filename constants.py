41a67cad781fc03292aa3e30ddc2bebc3c3c0a81
nepeat <nepeat@users.noreply.github.com>
2018-06-16 04:58:17 -0700

    Add reasoning comment to fee "highway robbery" constant.

--- constants.py
+++ constants.py
@@ -2,4 +2,6 @@ import os
 
 COIN_TO_SATOSHI = 1000000
 STATIC_FEE = float(os.environ.get("COIN_FEE", 0.01))
+
+# We should not be paying more than 5 coins as fees at all.
 HIGHWAY_ROBBERY = 5 * COIN_TO_SATOSHI
